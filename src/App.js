bb14eb6f9c6be9e679748a5dbb1ca91489213c0b
Aderinokun Oluwatobi <oluwatobi.aderinokun222@gmail.com>
2023-11-08 11:34:00 +0100

    pricing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Landingpage from './Pages/Landingpage/Landingpage';
 import Navigation from './Pages/Nav/Navigation';
 import Home from './Pages/Home/Home';
+import Pricings from './Pages/Pricing/Pricing';
 
 
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='/AboutUs'element={<Landingpage/>}/>
         <Route path='/product'element={<Landingpage/>}/>
         <Route path='/Pricing'element={<Landingpage/>}/>
+        <Route path='/Pricings'element={<Pricings/>}/>
       </Routes>
     </>
   );
