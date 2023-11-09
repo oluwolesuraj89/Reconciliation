dbe1bd1cd82bd14ae6edbfcc4a93541851f6f06b
Aderinokun Oluwatobi <oluwatobi.aderinokun222@gmail.com>
2023-11-09 10:03:02 +0100

    pricing-signup

--- src/Pages/Landingpage/Landingpage.js
+++ src/Pages/Landingpage/Landingpage.js
@@ -350,52 +350,9 @@ const data1 = [
               <button className={classes.demo}><i class='bx bx-play-circle'></i> Demo </button>
               <button className={classes.signUp}>Sign Up</button>
             </div>
-          </div>
-        </div>
-        <footer>
-          <div className={classes.container}>
-            <span className={classes.copyright}>
-              <img src={Logo} alt='logo' className={classes.logo}/>
-              <p>Copyright © 2023 Eazipay.</p>
-              <p>All rights reserved</p>
-              <icons className={classes.icons}>
-                <span><i class='bx bxl-instagram'></i></span>
-                <span><i class='bx bxl-twitter'></i></span>
-                <span><i class='bx bxl-linkedin'></i></span>
-                <span><i class='bx bxl-facebook'></i></span>
-              </icons>
-            </span>
-            <div className={classes.footerFlex}>
-              <span className={classes.span}>
-                <h5>Product</h5>
-                <NavLink to ={'#'}>Businesses</NavLink>
-                <NavLink to ={'#'}>Request Demo</NavLink>
-                <NavLink to ={'#'}>Pricing</NavLink>
-                
-              </span>
-              <span className={classes.span}>
-                <h5>Legal</h5>
-                <NavLink to ={'#'}>Privacy Policy</NavLink>
-                <NavLink to ={'#'}>Terms of Service</NavLink>
-                
-              </span>
-              <span className={classes.span}>
-                <h5>Resources</h5>
-                <NavLink to ={'#'}>FAQs</NavLink>
-                <NavLink to ={'#'}>Blog</NavLink>
-                <NavLink to ={'#'}>Career</NavLink>
-                <NavLink to ={'#'}>Customer Stories</NavLink>
-                
-              </span>
-              <span className={classes.span}>
-                <h5>Contact us</h5>
-                <p>eazipay@gmail.com</p>
-                <p>+234 816 878 9518</p>
-                <p className={classes.emaildetails}>
-                  Your email address
-                  <i>icon</i>
-                </p>
-              </span>
+            <div className={classes.mobileBtns }>
+              <button className={classes.getStarted}>Get started </button>
+              <button className={classes.learnMore}>Learn more</button>
             </div>
           </div>
         </div>
