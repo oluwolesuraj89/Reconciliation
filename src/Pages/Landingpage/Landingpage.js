80e5b17cf06b4bf31b2cb5e9ff15d3496c2e3d47
oluwolesuraj89 <oluwolesuraj98@gmail.com>
2023-11-09 10:45:44 +0100

    commited

--- src/Pages/Landingpage/Landingpage.js
+++ src/Pages/Landingpage/Landingpage.js
@@ -308,35 +308,19 @@ const data1 = [
               <div className='questions-cont'>
                   <div className='accordion'>
 
-                  <Accordion defaultActiveKey="0" flush>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+                    <Accordion defaultActiveKey="0" flush>
+                      {data1.map((items) => (
+                        <Accordion.Item eventKey="0" className={classes.items}>
+                          <Accordion.Header>{items.question}</Accordion.Header>
+                          <Accordion.Body>
+                          {items.answer}
+                          </Accordion.Body>
+                        </Accordion.Item>
+                      ))}
+                    </Accordion>
 
 
-                      {data1.map((items, i) => (
+                      {/* {data1.map((items, i) => (
                           <div className={classes.items} key={i} onClick={() => toggle1(i)}>
                               <div className={classes.accordionTitle}>
                                   <h4>{items.question}</h4>
@@ -346,7 +330,7 @@ const data1 = [
                                   {items.answer}
                               </div>
                           </div>
-                      ))}
+                      ))} */}
                   </div>
               </div>
             </div>
